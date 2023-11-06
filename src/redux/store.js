1b44dc5d9bb9db967e84c97866b70509ec9da54d
ailsoya <akalitata53@gmail.com>
2023-11-06 15:29:15 +0200

    1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['contacts']
+}
+
+const persistedContactsReducer = persistReducer(persistConfig, contactsReducer)
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedContactsReducer,
     filter: filterReducer,
   },
 })
+
+export const persistor = persistStore(store)
